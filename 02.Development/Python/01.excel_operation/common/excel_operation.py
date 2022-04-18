df896b3995082cfa0330df1fa4afdc4c092ab397
peter.yan <peter.yan@mediatek.com>
2022-04-18 17:11:12 +0800

    [ExcelOperation] Refactoring

--- 02.Development/Python/01.excel_operation/common/excel_operation.py
+++ 02.Development/Python/01.excel_operation/common/excel_operation.py
@@ -1,21 +1,65 @@
+from itertools import product
 import string
 import openpyxl
+from openpyxl.worksheet.worksheet import Worksheet
 import re
 
-class ExcelIndex:
+
+KEYINFO = ["Id", "Description", "Status", "ChapterId", "Chapter", "SectionId", "Section"]
+NOCOLUMNINX = -1
+
+"""
+   class ExcelParser
+"""
+class ExcelParser:
     def  __init__(self):
+        self.initEmpty()
+    
+    def  __init__(self, keyRow:int, originalKeyMap: dict, typeRow:int, originalProductTypeList: list):
+        self.initEmpty()
+        self.initialize(keyRow, originalKeyMap, typeRow, originalProductTypeList)
+        
+    """
+       Initialize method
+    """    
+    def initEmpty( self ):
         self.keyMap = {}
         self.typeMap = {}
-        
     
-    def setKeyMap( self, sheet, row, cellValueList: list):
-        self.__setMap( sheet, row, cellValueList, self.keyMap)
+    def initialize( self, keyRow:int, originalKeyMap: dict, typeRow:int, originalProductTypeList: list):
+        self._originalKeyMap = originalKeyMap.copy()  
+        self.keyRow = keyRow
+        self._originalProductTypeList = originalProductTypeList.copy()
+        self.typeRow = typeRow
             
-    def setTypeMap( self, sheet, row, cellValueList:list):
-        self.__setMap( sheet, row, cellValueList, self.typeMap)
+    """
+       END Initialize method
+    """ 
+    def createMap( self, sheet: Worksheet):
+        self.setKeyMap(sheet, self.keyRow, self._originalKeyMap)
+        self.setTypeMap(sheet, self.typeRow, self._originalProductTypeList)
     
-    def __setMap(self, sheet, row, cellValueList: list, dataMap: map):
-        index = self.__getColumnsIdxBasedOnList( sheet, row, cellValueList)
+    """
+      the input of These Group function is <KeyInfo, ExcelColumn> and it will create < KeyInfo, ExcelIndex>
+    """    
+    def setKeyMap( self, sheet, row, keyMap: dict):
+        cellValueList = []
+        for k in KEYINFO:
+            val = keyMap.get(k)
+            
+            if val == None or val == "":
+                print("Please note the key = " + k + " is NULL or empty string." )
+                val = NOCOLUMNINX
+            cellValueList.append( val )
+        index = self.__getColumnsIdxBasedOnList( sheet, row, cellValueList)   
+                    
+        self.__setMap( sheet, row, index, KEYINFO, self.keyMap)
+       
+    def setTypeMap( self, sheet, row, productTypeList: list):
+        index = self.__getColumnsIdxBasedOnList( sheet, row, productTypeList)  
+        self.__setMap( sheet, row, index, productTypeList, self.typeMap)
+    
+    def __setMap(self, sheet, row, index: list, cellValueList: list, dataMap: map):
         i = 0
         for key in cellValueList:
             dataMap[key] =index[i]
@@ -35,28 +79,72 @@ class ExcelIndex:
                 
         return result 
     
+    def getKeyIndex(self, key):
+        return self.keyMap[key] 
+    def getProductTypeList(self):
+        return self._originalProductTypeList
     
+    """
+        The List of keyColumn should follow the sequence: status, capability, id, desc, section.
+        The Priority value depends on the List of productColumn
+    """
+    def parse( self, fileName, sheetName, dataRow) :
+        excel = openpyxl.load_workbook(fileName)
+        sheet = excel[sheetName]
+        print("[ExcelParser.parse][BEGIN]: " + fileName +" MAX ROW = " + str(sheet.max_row))
+    
+        result = []
 
+        for r in range( dataRow, sheet.max_row + 1):
+            item = Item(fileName, self.getProductTypeList() )
+            item.setKeyInfo( sheet[r], self.keyMap, self.typeMap)
+
+            result.append(item)
+
+        print("[ExcelParser.parse][END]: " + fileName)
+        excel.close()
+        return result
+
+"""
+   END class ExcelParser
+"""
+   
 """
   Class Items
 """
 class Item:
-    def __init__(self, fileName, status, capability, id, desc, section):
-        self.status = status
-        self.capability = capability
-        self.id = id
-        self.desc = desc
-        self.section = section
+    def __init__(self, fileName, productType: list):
         self.fileName = fileName
-        self.priority = []
-        self.type = []
+        self.keyValue = []
+        self.type = productType.copy()
+        self.priority = []  
     def __str__(self):
      return self.id
-
+    
+    def setKeyInfo( self, row, keyMap: dict, typeMap: dict):
+        # set key 
+        for k in KEYINFO:
+            idx = keyMap[k]
+            if( idx == -1 ):
+                self.keyValue.append(None)
+            else:
+                self.keyValue.append( row[idx].value )
+        #add priority
+        for t in self.type:
+            idx = typeMap[t]
+            self.addPriority( row[idx].value )
+        
     def addPriority( self, priority):
         self.priority.append(priority)
     def addType( self, t):
         self.type.append(t)
+        
+    def getKeyInfo( self, key):
+        for i in range( len(KEYINFO) ):
+            if KEYINFO[i] == key:
+                return self.keyValue[i]
+           
+        return None
     
 """
   End Class Items
@@ -91,23 +179,6 @@ def getColumnsIdxBasedOnString( sheet, row, cellValue: str):
             return col.col_idx - 1
 
 
-"""
- The List of keyColumn should follow the sequence: status, capability, id, desc, section.
- The Priority value depends on the List of productColumn
-"""
-def getExcelRows( fileName, sheet, dataRow, keyIdx: list, priorityIdx) -> list[Item]:
-    print("[getExcelRows][BEGIN]: " + fileName +" ROW = " + str(sheet.max_row))
-    if( len(keyIdx) != 5):
-        raise Exception("keyColumn should only have 4 elements")
-    result = []
-    for r in range( dataRow, sheet.max_row):
-        id = sheet[r][keyIdx[1]].value
-        if id: 
-            item = Item(fileName, sheet[r][keyIdx[0]].value, sheet[r][keyIdx[1]].value, sheet[r][keyIdx[2]].value, sheet[r][keyIdx[3]].value, sheet[r][keyIdx[4]].value)
-            item.addPriority(sheet[r][priorityIdx[0]].value)
-            item.addPriority(sheet[r][priorityIdx[1]].value)
-            result.append(item)
-    print("[getExcelRows][END]: " + fileName)
-    return result
+
    
 
--- 02.Development/Python/01.excel_operation/softbank/softbank_excel.py
+++ 02.Development/Python/01.excel_operation/softbank/softbank_excel.py
@@ -1,43 +1,50 @@
 
+from itertools import product
+from pickle import TRUE
 from typing import Dict
 from common import excel_operation
 from common import file_operation
 import openpyxl
 import time
 import datetime
+import os
 
 
+"""
+getExcelParser
+""" 
+def getExcelParser( fileName, sheetName, keyMap: dict, productType: list) -> excel_operation.ExcelParser:
+    excel = openpyxl.load_workbook(fileName)
+    sheet = excel[sheetName]
+    result = excel_operation.ExcelParser(4, keyMap, 3, productType)
+    result.createMap(sheet)
+    return result
+
+"""
+END getExcelParser
+"""
 """
 getItemsFromExcel
-"""
+""" 
+#End getExcelIndex Function
 #addToMap Function
 def addToMap( items: list[ excel_operation.Item ], itemMap: dict ):
     for item in items:
-        if item.status != "Delete":
-           itemMap[item.id] = item
+        id = item.getKeyInfo("Id")
+        if id != None:
+            status = item.getKeyInfo("Status")
+            if status != "Delete":
+                itemMap[id] = item
 #End addToMap Function
-def getItemsFromExcel( fileName: list, sheetName, keyColumnName: list, productType: list ):
-    #2.1 Get Critical Column
-    #capability, id, desc, section, priority  
-    excel = openpyxl.load_workbook(fileName[0])
-    sheet = excel[sheetName]
-    keyIdx = excel_operation.getColumnsIdxBasedOnList(sheet, 4, keyColumnName)
-    
-    productIdx=  excel_operation.getColumnsIdxBasedOnList(sheet,3, productType)
-
+def getItemsFromExcel( fileName: list, sheetName, excelParser: excel_operation.ExcelParser ):
     result = {}
   
     beginT = str(datetime.datetime.now())
-#fileName = ["D:\\02.Operator\\02.Softbank\\02.Operator Terminal Requirements\\OTR-2022.Q2.Q3\\OTR-20210730_FullPackage\\02_Conformance_sheet\\OTR-MTC-MTC_THP-CONF-RevD01-20210226_E.xlsx"]
+    fileName = ["D:\\02.Operator\\02.Softbank\\02.Operator Terminal Requirements\\OTR-2022.Q2.Q3\\OTR-20210730_FullPackage\\02_Conformance_sheet\\OTR-MTC-MTC_RF-CONF-RevD01-20210226_E.xlsx"]
     for f in fileName:
-        print("open: " + f)
-        excel = openpyxl.load_workbook(f)
-        sheet = excel[sheetName]
-        items = excel_operation.getExcelRows(f, sheet, 5, keyIdx, productIdx)
+        items = excelParser.parse(f, sheetName, 5)
         print("addToMap: " + f)
         addToMap(items, result)
-
-        excel.close()
         print("finish: " + f)
     #items.append( getItems(sheet, keyColumn, productColumn) )
     endT = str(datetime.datetime.now())
@@ -66,15 +73,15 @@ def outputExcel( outputFileName, productType: list, itemMap: dict  ):
 
     r = 2
     for item in itemMap.values():
-    
-        sheet.cell(row=r, column=1).value = item.id
-        sheet.cell(row=r, column=2).value = item.desc
-        sheet.cell(row=r, column=3).value = item.section
-        c = 4
+        sheet.cell(row=r, column=1).value = item.getKeyInfo("Id")
+        sheet.cell(row=r, column=2).value = item.getKeyInfo("Description")
+        sheet.cell(row=r, column=3).value = item.getKeyInfo("Section")
+        sheet.cell(row=r, column=4).value = item.fileName
+        c = 5
         for p in item.priority:
             sheet.cell(row=r, column=c).value = p
             c += 1
-        sheet.cell(row=r, column=c).value = item.fileName
+        
         r += 1
    
     wb.save(outputFileName)
@@ -90,13 +97,26 @@ def softbank_parser():
     #1. Get fileName in the folder
     fileName = file_operation.getFileNameList(folder, prefix)
     #2. Get each items in the files
-    keyColumnName = ["Status", "Capability", "Requirement ID", "Function\n(Description)", "Reference\n(Section Name)", ]
-    productType = ["M2M", "Manufacturer brand M2M"]
     sheetName="Conformance Sheet"
-    itemMap = getItemsFromExcel(fileName, sheetName, keyColumnName, productType)
+    keyColumnName = {
+        "Id": "Requirement ID", 
+        "Description":  "Function\n(Description)", 
+        "Status": "Status", 
+        #"ChapterId":"", 
+        "Chapter": "Capability", 
+        "SectionId": "Reference\n(Section Info.)", 
+        "Section": "Reference\n(Section Name)"
+    }
+    productType = ["M2M", "Manufacturer brand M2M"]
+    #2.1 Get Critical Column
+    excelIndex = getExcelParser( fileName[0], sheetName, keyColumnName, productType)
+    itemMap = getItemsFromExcel(fileName, sheetName, excelIndex)
     #3. Output to one Excels
-    outputFileName = "D:\\99.Source\\01.Python\\99.PythonStudy\\result\\softbank_item.xlsx"
+    outputFolder = ".\\result\\"
+    if os.path.isdir(outputFolder) != True:
+        os.makedirs(outputFolder)
     
+    outputFileName = outputFolder+"softbank_item.xlsx"
     outputExcel(outputFileName, productType, itemMap)
 
 if __name__ == '__main__':
