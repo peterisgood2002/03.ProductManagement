0977bb3724aee6833ecfaae68ea6deca06ec3651
peter.yan <peter.yan@mediatek.com>
2022-05-09 17:29:09 +0800

    [PMS_Backend] index page

--- 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
@@ -1,3 +1,9 @@
+import django
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index( request):
+    password = encrypt('U0654t/6')
+    
+    return HttpResponse("Hello World:" + password)
