68fde348dc0984f2bde46b538c943cb8267daa87
peter.yan <peter.yan@mediatek.com>
2022-05-11 18:33:52 +0800

    [Others] get CNN 5 Things

--- 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
@@ -1,12 +1,29 @@
-import django
-from django.http import HttpResponse
-from django.shortcuts import render
-import os
 
-#from pms_be.models.e_employee import EEmployee
-# Create your views here.
-from pms_be.models.e_operator import EComplianceVersion
+from django.http import HttpResponse
+from pms_be.models.e_employee import EEmployee
+
+import requests
+from bs4 import BeautifulSoup, Tag
 
 def index( request):
-    EComplianceVersion()
     return HttpResponse("Hello World:")
+
+def getCNN5():
+     #e = EEmployee( id = 1, english_name = "test")
+     #e.save()
+     
+    r = requests.get("https://edition.cnn.com/audio/podcasts/5-things", verify=False)
+    
+    soup = BeautifulSoup(r.text, 'html.parser')
+     
+    f = open("C:\\Users\\mtk26734\\Desktop\\audio.txt", "w")
+    for tag in  soup.find(id = "episodes").find_all("audio-player-wc"):
+        t = Tag.__copy__(tag)
+        link = t.attrs.get("src") + "\n"
+        f.write(link)
+        
+    f.close()
+    
+    
+    
+    
