844df8c7a573d4422dbf18819f892a6b6d19da82
peter.yan <peter.yan@mediatek.com>
2022-05-12 14:21:16 +0800

    [Others] get CNN 5 Things

--- 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_be/views.py
@@ -1,4 +1,7 @@
 
+from datetime import datetime
+import glob
+import os
 from django.http import HttpResponse
 from pms_be.models.e_employee import EEmployee
 
@@ -9,20 +12,32 @@ def index( request):
     return HttpResponse("Hello World:")
 
 def getCNN5():
-     #e = EEmployee( id = 1, english_name = "test")
-     #e.save()
-     
+ 
+    folder = "C:\\Users\\mtk26734\\Music\\"
+    #folder = "C:\\Users\\mtk26734\\Desktop\\TEST\\"
+    #1. Get link from CNN 5 things 
     r = requests.get("https://edition.cnn.com/audio/podcasts/5-things", verify=False)
     
     soup = BeautifulSoup(r.text, 'html.parser')
      
-    f = open("C:\\Users\\mtk26734\\Desktop\\audio.txt", "w")
+    f = open(folder+ "audio.txt", "w")
     for tag in  soup.find(id = "episodes").find_all("audio-player-wc"):
         t = Tag.__copy__(tag)
         link = t.attrs.get("src") + "\n"
         f.write(link)
         
     f.close()
+    
+    #2. remove files
+    t = datetime.now().time()
+    if t.hour < 9:
+        if os.path.exists( folder) :
+            files = glob.glob(folder + "*.mp3")
+            for file in files:
+                os.remove(file)
+
+def test():
+    folder = "C:\\Users\\mtk26734\\Desktop\\TEST\\"
     
     
     
