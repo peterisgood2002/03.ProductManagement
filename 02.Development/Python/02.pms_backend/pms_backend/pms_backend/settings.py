152a35c1fc384289ee62350508dab666278de7d2
peter.yan <peter.yan@mediatek.com>
2022-05-19 10:23:29 +0800

    [Excel_Common] start test

--- 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pms_be.apps.PmsBeConfig',
-    'pms_operator.apps.PmsOperatorConfig'
+    'pms_operator.apps.PmsOperatorConfig',
     'excel_common.apps.ExcelCommonConfig'
 ]
 
