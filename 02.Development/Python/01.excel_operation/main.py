db0105ca8c11aceba0e847d425f0a0f3d1c14699
peter.yan <peter.yan@mediatek.com>
2022-05-24 15:01:52 +0800

    [Excel_Operation] bug

--- 02.Development/Python/01.excel_operation/main.py
+++ 02.Development/Python/01.excel_operation/main.py
@@ -3,7 +3,7 @@ from softbank.softbank_excel import softbank_parser
 import tmo.tmo_excel
 
 if __name__ == '__main__':
-    #softbank_parser()
+    softbank_parser()
     
     
-    tmo.tmo_excel.tmo_parser()
+    #tmo.tmo_excel.tmo_parser()
