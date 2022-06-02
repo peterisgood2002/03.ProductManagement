e44a3c44abc0cbfb1bd88a58990152c7f40061ef
peter.yan <peter.yan@mediatek.com>
2022-06-02 17:55:57 +0800

    [PMS_Operator] ATT compliance function

--- 02.Development/Python/02.pms_backend/pms_backend/pms_operator/operator/att_excel.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_operator/operator/att_excel.py
@@ -1,5 +1,51 @@
-from excel_common.excel_operation import ExcelParser, KEYINFO
+from email import utils
+import logging
+import os
+import shutil
+import string
 
-keyMap = {
+import openpyxl
+from excel_common.excel_operation import ExcelParser, KEYINFO
+from excel_common.excel_operation import Item
+from .utils import INPUT_FOLDER
+
+logger = logging.getLogger("ExcelParser")
+
+def _getATTCompliance( fileName, sheetName, excel ) -> list[Item]:
+    logging.info("[_getATTCompliance][Begin]")
+    keyMap = {
+        KEYINFO.Id: "Requirement TAG", 
+        KEYINFO.Description:  "Description", 
+        KEYINFO.Status: "Status", 
+        KEYINFO.ChapterId:"Chapter No.", 
+        KEYINFO.Chapter: "Chapter Name", 
+        KEYINFO.SectionId: "Section No.", 
+        KEYINFO.Section: "Section Name",
+        KEYINFO.Priority: "Priority",
+    }
+        
+    sheet = excel[sheetName]
     
-}
+    parser = ExcelParser(keyRow=13, originalKeyMap = keyMap)
+    parser.createMap(sheet)
+    
+    result = parser.parseExcel( fileName, 14, sheet)
+    logging.info("[_getATTCompliance][End] result = {0}", len(result))
+    return result 
+
+def _getATTItems( fileName):
+    
+    excel = openpyxl.load_workbook( fileName )
+    items = _getATTCompliance(fileName, "COMPLIANCE MATRIX", excel)
+
+
+#
+def att_excel_parser(srcName: string, version):
+    if os.path.isdir( INPUT_FOLDER) == False:
+        os.makedirs(INPUT_FOLDER)
+        
+    #1. Move file to folder
+    desName = INPUT_FOLDER + os.path.basename(srcName)
+    #shutil.move(srcName, desName)
+    #2. Parse file and Get Item
+    _getATTItems(desName)
--- 02.Development/Python/02.pms_backend/pms_backend/pms_operator/testCase/testATT.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_operator/testCase/testATT.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
+from pms_operator.operator.att_excel import att_excel_parser
+
 
 class ATTTest(TestCase):
     def test(self):
-        print('TEST')
+        
+        att_excel_parser("D:\\02.Operator\\01.AT&T\\04.FeatureRequirements_13289.Compliance Metrix\\v19.3\\13289 19.3.xlsx",19.3)
