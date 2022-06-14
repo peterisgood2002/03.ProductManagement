9407dd09c5fd6ce8ac1130d1c20deb5a3bc28425
peter.yan <peter.yan@mediatek.com>
2022-06-14 11:37:12 +0800

    [PMS_Backend] sort out models

--- 02.Development/Python/02.pms_backend/pms_backend/pms_be/models/e_operator.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_be/models/e_operator.py
@@ -11,13 +11,13 @@ class EArea(models.Model):
         managed = False
         db_table = 'e_area'
 
+
 class EOperator(models.Model):
     name = models.CharField(max_length=45, db_collation='utf8_general_ci', blank=True, null=True)
-    create_date = models.DateField(blank=True, null=True)
-    update_date = models.DateField(blank=True, null=True)
-    spm = models.ForeignKey(EEmployee, models.DO_NOTHING, db_column='spm', blank=True, null=True)
     area = models.ForeignKey(EArea, models.DO_NOTHING)
     url = models.TextField(blank=True, null=True)
+    create_date = models.DateField(blank=True, null=True)
+    update_date = models.DateField(blank=True, null=True)
 
     class Meta:
         managed = False
