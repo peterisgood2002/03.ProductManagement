980cca46640411fc4eb7e9683282d0736f92b777
peter.yan <peter.yan@mediatek.com>
2022-06-29 19:00:19 +0800

    Fixed build break

--- 02.Development/Python/02.pms_backend/pms_backend/pms_be/tests.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_be/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 
 from pms_be.views import getCNN5
 from pms_be.models.e_employee import EEmployee
+>>>>>>> 94aec3d (Fixed build break)
 # Create your tests here.
 
 class CNNTest(TestCase):
