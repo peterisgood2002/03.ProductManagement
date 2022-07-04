b3139833beb46a287926a0c90372d6ca8b356853
peter.yan <peter.yan@mediatek.com>
2022-07-04 08:36:33 +0800

    [Security] Regenerate Secret Key

--- 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w@m&c*5y*35qqvvw^2m$%ppiy_nv593zd8g4)y%2%5vsvc#x@u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
