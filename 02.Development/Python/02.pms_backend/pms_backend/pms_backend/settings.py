cab21791f4aadc4adeacd29033a37789a7a3afc0
peter.yan <peterisgood2002@gmail.com>
2024-01-21 12:59:49 +0800

    [PMS_MILESTONE] python manage.py startapp pms_milestone

--- 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
+++ 02.Development/Python/02.pms_backend/pms_backend/pms_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,110 +21,105 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","*","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "*", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'pms_be.apps.PmsBeConfig',
-    'pms_operator.apps.PmsOperatorConfig',
-    'excel_common.apps.ExcelCommonConfig',
-    'pms_dbmodel.apps.PmsDbmodelConfig',
-    'pms_customer.apps.PmsCustomerConfig',
-    'pms_platform.apps.PmsPlatformConfig',
-    'pms_project.apps.PmsProjectConfig'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "pms_be.apps.PmsBeConfig",
+    "pms_operator.apps.PmsOperatorConfig",
+    "excel_common.apps.ExcelCommonConfig",
+    "pms_dbmodel.apps.PmsDbmodelConfig",
+    "pms_customer.apps.PmsCustomerConfig",
+    "pms_platform.apps.PmsPlatformConfig",
+    "pms_project.apps.PmsProjectConfig",
+    "pms_milestone.apps.PmsMilestoneConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'pms_backend.urls'
+ROOT_URLCONF = "pms_backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'pms_backend.wsgi.application'
+WSGI_APPLICATION = "pms_backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'NAME': 'django_admin',
-        'USER': 'root',
-        'PASSWORD': os.getenv("SQL_PWD"),
-        'TEST':{
-            'MIGRATE':True    
-        },
-        'MIGRATE':False
-        
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "NAME": "django_admin",
+        "USER": "root",
+        "PASSWORD": os.getenv("SQL_PWD"),
+        "TEST": {"MIGRATE": True},
+        "MIGRATE": False,
     },
-    'pms_db': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'NAME': 'pms',
-        'USER': 'root',
-        'PASSWORD': os.getenv("SQL_PWD"),
-        'TEST':{
-            'MIGRATE':False    
-        },
+    "pms_db": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "NAME": "pms",
+        "USER": "root",
+        "PASSWORD": os.getenv("SQL_PWD"),
+        "TEST": {"MIGRATE": False},
     },
-    
 }
 
-DATABASE_ROUTERS = ['pms_backend.database_router.PmsDBRouter']
+DATABASE_ROUTERS = ["pms_backend.database_router.PmsDBRouter"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -131,9 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -143,37 +139,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
         },
-        #SQL Debug
-        #'django.db.backends': {
-        #    'level': 'DEBUG',
-        #    'handlers': ['console'],
-        #}
-        
+        # SQL Debug
+        # "django.db.backends": {
+        #    "level": "DEBUG",
+        #    "handlers": ["console"],
+        # },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
     },
 }
